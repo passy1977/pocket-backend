8e40026378d8759b42c564cd6b40dd07194506b3
Antonio Salsi <antoniosalsi@docker.salsi.local>
2025-05-22 23:48:46 +0200

    feat: continue to develop container

--- build_and_start_docker_image.sh
+++ build_and_start_docker_image.sh
@@ -1,69 +1,84 @@
 #!/bin/bash
 
-#sudo rm -fr mysql_data pocket5_config
+sudo rm -fr docker_data
 
 NETWORK=pocket5_network
 
 if docker network inspect $NETWORK &> /dev/null; then
   echo "'$NETWORK' ready"
 else
-  if test -f "/var/lib/docker/network/$(docker network create --format '{{.ID}}' $NETWORK)"; then
+  #if test -f "$(sudo docker network ls --format "{{ .Name }}" | grep  pocket5_network)"; then
+  if [ "$(sudo docker network ls --format "{{ .Name }}" | grep  $NETWORK)" = $NETWORK ]; then
     echo "'$NETWORK' ready but non started manually"
   else
     echo "Create network '$NETWORK'"
-    docker network create ai_network
+    sudo docker network create $NETWORK
   fi
 fi
 
+if [ ! -d docker_data ]; then
+  mkdir -p docker_data
+fi
 
-if [ ! -d mariadb_data ]; then
-  mkdir -p mariadb_data;
+if [ ! -d docker_data/mariadb ]; then
+  mkdir -p docker_data/mariadb;
 
   read -s -p "Insert passwd for MariaDB root user: " MARIADB_ROOT_PWD
   echo
-  echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PWD';" > create_root_user.sql
-  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> create_root_user.sql
-  echo "FLUSH PRIVILEGES;" >> create_root_user.sql
+  echo "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PWD';" > docker_data/mariadb/create_root_user.sql
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> docker_data/mariadb/create_root_user.sql
+  echo "FLUSH PRIVILEGES;" >> docker_data/mariadb/create_root_user.sql
 
   echo "MARIADB_ROOT_PWD=$MARIADB_ROOT_PWD" > .env
 fi
 
-if [ ! -d pocket5_config ]; then
-  mkdir -p pocket5_config
-
-  if [ -n "$MARIADB_ROOT_PWD" ]; then
-    until docker exec -it db mysqladmin -u root -p$MARIADB_ROOT_PWD ping > /dev/null 2>&1; do
-      echo "Waiting for MySQL to start..."
-      sleep 5
-    done
-
-    # Aggiorna il file YAML con la variabile $MARIADB_ROOT_PWD
-    new_yaml_content=$(echo "$yaml_content" | sed -e "s/password: .*/password: \"$MARIADB_ROOT_PWD\"/")
-    echo "$new_yaml_content" > scripts/pocket5_config.yaml
-
-    docker exec -i db mysql -u root -p$MARIADB_ROOT_PWD < create_root_user.sql
-    rm create_root_user.sql
-  else
-    echo "ERROR: $MARIADB_ROOT_PWD is not set. Please provide the password."
+if [ ! -d docker_data/pocket5 ]; then
+  if [ -z "$MARIADB_ROOT_PWD" ]; then
+      echo "MARIADB_ROOT_PWD empty"
+      exit 1
   fi
+
+  mkdir -p docker_data/pocket5
+
+  read -s -p "AES CBC IV (16 char are mandatory): " AES_CBC_IV
+  echo
+  if [[ ${#AES_CBC_IV} -ne 16 ]]; then
+      echo "AES CBC IV don't contain a string of 16 chars" 1>&2
+      exit 1
+  fi
+
+  read -s -p "Auth email: " AUTH_USER  
+  echo
+  read -s -p "Auth passwd (32 char are mandatory): " AUTH_PASSWD
+  echo
+
+  if [[ ${#AUTH_PASSWD} -ne 32 ]]; then
+      echo "Auth passwd don't contain a string of 32 chars" 1>&2
+      exit 1
+  fi
+
+sed -e "s/MARIADB_ROOT_PWD/$MARIADB_ROOT_PWD/g" \
+    -e "s/AES_CBC_IV/$AES_CBC_IV/g" \
+    -e "s/AUTH_USER/$AUTH_USER/g" \
+    -e "s/AUTH_PASSWD/$AUTH_PASSWD/g" scripts/pocket5-config.yaml > docker_data/pocket5/pocket5-config.yaml
 
 fi
 
 
-docker compose up -d
+sudo docker compose up -d
 
 if [ -n "$MARIADB_ROOT_PWD" ]; then
 
-  until docker exec -it db mysqladmin -u root -p$MARIADB_ROOT_PWD ping > /dev/null 2>&1; do
+  until sudo docker exec -it db mysqladmin -u root -p$MARIADB_ROOT_PWD ping > /dev/null 2>&1; do
     echo "Waiting for MySQL to start..."
     sleep 5
   done
 
-  docker exec -i db mysql -u root -p$MARIADB_ROOT_PWD < create_root_user.sql
+  sudo docker exec -i db mysql -u root -p$MARIADB_ROOT_PWD < docker_data/mariadb/create_root_user.sql
 
-  rm create_root_user.sql
+  rm docker_data/mariadb/create_root_user.sql
 else
-  echo "ERROR: $MARIADB_ROOT_PWD is not set. Please provide the password."
+  echo "ERROR: $MARIADB_ROOT_PWD is not set. Please provide the password" 1>&2
 fi
 
 echo "End!"
